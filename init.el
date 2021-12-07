9568b3877c17d4825538b326087533de3a99b024
hujianfeng <hujianfeng@myoas.com>
2021-12-07 20:59:55 +0800

    local elpa.

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 ;;       in Emacs and init.el will be generated automatically!
 
 ;; You will most likely need to adjust this font size for your system!
-(defvar efs/default-font-size 180)
-(defvar efs/default-variable-font-size 180)
+(defvar efs/default-font-size 140)
+(defvar efs/default-variable-font-size 140)
 
 ;; Make frame transparency overridable
 (defvar efs/frame-transparency '(95 . 95))
@@ -46,11 +46,12 @@
 ;;                          ("melpa-stable" . "https://stable.melpa.org/packages/")
 ;;                          ("org" . "https://orgmode.org/elpa/")
 ;;                          ("elpa" . "https://elpa.gnu.org/packages/")))
-;; (setq package-archives '(("myelpa" . "~/myelpa-mirror/")))
-(add-to-list 'package-archives
-  ;; choose either the stable or the latest git version:
-  ;; '("melpa-stable" . "http://stable.melpa.org/packages/")
-  '("melpa-unstable" . "http://melpa.org/packages/"))
+(setq package-archives '(("myelpa" . "~/myelpa-mirror/")))
+;; geiser
+;; (add-to-list 'package-archives
+;;   ;; choose either the stable or the latest git version:
+;;   ;; '("melpa-stable" . "http://stable.melpa.org/packages/")
+;;   '("melpa-unstable" . "http://melpa.org/packages/"))
 
 (package-initialize)
 (unless package-archive-contents
